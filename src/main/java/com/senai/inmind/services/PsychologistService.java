571b1450f94cf82758e54f8a047c4286ca17c6b7
LuaraSouto <109703333+LuaraSouto@users.noreply.github.com>
2023-11-29 15:45:26 -0300

    Add files via upload

--- src/main/java/com/senai/inmind/services/PsychologistService.java
+++ src/main/java/com/senai/inmind/services/PsychologistService.java
@@ -1,6 +1,7 @@
 package com.senai.inmind.services;
 
 import java.util.List;
+import java.util.stream.Collector;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
